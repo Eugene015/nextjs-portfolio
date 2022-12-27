3803dad0d65ae7ad545fbc3b56edf6c5a09da6ca
Eugene015 <egavriluk@gmail.com>
2022-12-27 13:34:45 +0100

    skills section aded

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import Image from "next/image";
+import SkillCard from "./ui/skillCard";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <div className="w-full lg:h-screen p-2">
+      <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+          Skills
+        </p>
+        <h2 className="py-4">What I Can Do</h2>
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
+          <SkillCard skill="HTML" img="html" />
+          <SkillCard skill="CSS" img="css" />
+          <SkillCard skill="Tailwind" img="tailwind" />
+          <SkillCard skill="JS" img="javascript" />
+          <SkillCard skill="ReactJS" img="react" />
+          <SkillCard skill="NextJS" img="nextjs" />
+          <SkillCard skill="GitHub" img="github1" />
+          <SkillCard skill="Firebase" img="firebase" />
+          <SkillCard skill="MongoDB" img="mongo" />
+          <SkillCard skill="Express" img="express" />
+          <SkillCard skill="NodeJS" img="node" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
