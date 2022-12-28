202ffc8a28953af142b62d83530d67f9c4d5b07f
Eugene015 <egavriluk@gmail.com>
2022-12-28 16:28:34 +0100

    list style enhanc

--- components/About.jsx
+++ components/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import AboutImg from "../public/assets/about_pic.jpg";
 
 const About = () => {
@@ -35,25 +36,91 @@ const About = () => {
             By now, I have learned up-to-date frontend development stack (HTML,
             CSS, JavaScript, React.js). What I can do now:
           </p>
-          <ul className="py-2">
-            <li className="cursor-default">
-              - write basic react markup structure of React SPA;
+          <ul className="py-2 ml-8">
+            <li className="cursor-default  flex space-x-3">
+              <svg
+                class="flex-shrink-0 w-5 h-5 mt-1 text-green-500 dark:text-green-400"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              <span>write basic react markup structure of React SPA;</span>
             </li>
-            <li className="cursor-default py-2">
-              - write different forms (sign-in, signups, text, select,
-              multi-select, radio, etc. inputs), use React hooks to handle data
-              from it, and store data in different storages;
+            <li className="cursor-default py-2  flex space-x-3">
+              <svg
+                class="flex-shrink-0 w-5 h-5 mt-1 text-green-500 dark:text-green-400"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              <span>
+                write different forms (sign-in, signups, text, select,
+                multi-select, radio, etc. inputs), use React hooks to handle
+                data from it, and store data in different storages;
+              </span>
             </li>
-            <li className="cursor-default py-2">
-              - fetch data from APIs, use appropriate hooks, and use objects and
-              arrays to display that data in the application interface;
+            <li className="cursor-default py-2 flex space-x-3">
+              <svg
+                class="flex-shrink-0 w-5 h-5 mt-1 text-green-500 dark:text-green-400"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              <span>
+                fetch data from APIs, use appropriate hooks, and use objects and
+                arrays to display that data in the application interface;
+              </span>
             </li>
-            <li className="cursor-default py-2">
-              - create sorting, filtering, and searching functionality for
-              fetched data;
+            <li className="cursor-default py-2 flex space-x-3">
+              <svg
+                class="flex-shrink-0 w-5 h-5 mt-1 text-green-500 dark:text-green-400"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              <span>
+                create sorting, filtering, and searching functionality for
+                fetched data;
+              </span>
             </li>
-            <li className="cursor-default py-2">
-              - do other useful things with React and JavaScript.
+            <li className="cursor-default py-2 flex space-x-3">
+              <svg
+                class="flex-shrink-0 w-5 h-5 mt-1 text-green-500 dark:text-green-400"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              <span>do other useful things with React and JavaScript.</span>
             </li>
           </ul>
           <p className="py-2 text-gray-600">
@@ -61,9 +128,11 @@ const About = () => {
             applications, that could add significant value to company business
             products.
           </p>
-          <p className="py-2 text-gray-600 underline cursor-pointer">
-            Let`s see my latest works
-          </p>
+          <div className="flex items-center justify-center md:block">
+            <Link href="/#projects">
+              <button className="mt-6">Let`s see my latest works</button>
+            </Link>
+          </div>
         </div>
         <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
           <Image src={AboutImg} width="467" height="576" alt="/" />
